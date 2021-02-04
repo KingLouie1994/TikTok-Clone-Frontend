4f0e08996dd0e0ac9e4c928d463824f6fb19436e
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-02-04 13:15:23 +0100

    added basic styling to footer and installed material ui

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,10 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <div className="footer">
-
+            <div className="footer__text">
+                <h3>@ChannelName</h3>
+                <p>Here will be the description</p>
+            </div>
         </div>
     )
 }
