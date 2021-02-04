f0e000a91cfdfd87026de7b95ae364c3940b3ec7
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-02-04 13:26:56 +0100

    Using an Icon from Material UI and installed react-ticker

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,24 @@
+// Imports from third party libraries
+import Ticker from "react-ticker";
+
 // Imports for styling
 import "./Footer.css";
 
+// Imports from Material UI
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+
 const Footer = () => {
-    return (
-        <div className="footer">
-            <div className="footer__text">
-                <h3>@ChannelName</h3>
-                <p>Here will be the description</p>
-            </div>
+  return (
+    <div className="footer">
+      <div className="footer__text">
+        <h3>@ChannelName</h3>
+        <p>Here will be the description</p>
+        <div className="footer__ticker">
+          <MusicNoteIcon className="footer__icon" />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
